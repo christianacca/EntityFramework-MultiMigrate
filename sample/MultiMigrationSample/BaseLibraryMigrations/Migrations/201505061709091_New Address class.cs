3cfdc0a9f57760676eb0f02e81f055c5239ceff5
ccrowhurstram <ccrowhurst@ramplc.com>
2015-05-06 18:24:13 +0100

    docs(MultiMigrationSample): demonstrate update to upstream libray taken "as is"

---
+++ sample/MultiMigrationSample/BaseLibraryMigrations/Migrations/201505061709091_New Address class.cs
@@ -0,0 +1,30 @@
+namespace CcAcca.BaseLibraryMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewAddressclass : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "BaseLib.Addresses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Line1 = c.String(maxLength: 500),
+                        Line2 = c.String(maxLength: 500),
+                        Line3 = c.String(maxLength: 500),
+                        Line4 = c.String(maxLength: 500),
+                        Postcode = c.String(maxLength: 500),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("BaseLib.Addresses");
+        }
+    }
+}
---
+++ sample/MultiMigrationSample/LibraryMigrations/Migrations/201505061717249_Merge BaseLibrary 1.cs
@@ -0,0 +1,10 @@
+using System.Data.Entity.Migrations;
+
+namespace CcAcca.LibraryMigrations.Migrations
+{
+    public partial class MergeBaseLibrary1 : DbMigration
+    {
+        public override void Up() { }
+        public override void Down() { }
+    }
+}
